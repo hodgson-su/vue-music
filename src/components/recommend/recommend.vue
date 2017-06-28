1074865ba72b159f653f5ffa7aa61044e5078c3d
htz4004 <htz4004@126.com>
2017-06-28 17:31:42 +0800

    6/28

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="recommend" ref="recommend">
+  	<scroll ref="scroll" class="recommend-content" :data="discList">
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
